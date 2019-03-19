06c6db75c1e9a8aac48b8476672fb9184efb43b0
RoyiJourno <royijourno@gmail.com>
2019-03-19 11:32:44 +0200

    דף התחברות. כרגע עובר רק הLogin

---
+++ app/src/main/java/com/example/royi/racebet/User.java
@@ -0,0 +1,41 @@
+package com.example.royi.racebet;
+import java.util.List;
+
+public class User {
+    private String name;
+    private String phone;
+    private String photoPath;
+    private List<String> groupList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public List<String> getGroupList() {
+        return groupList;
+    }
+
+    public void setGroupList(List<String> groupList) {
+        this.groupList = groupList;
+    }
+}
