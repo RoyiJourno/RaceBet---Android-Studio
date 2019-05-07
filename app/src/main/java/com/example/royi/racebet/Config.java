9ec9349cac0be54ac55650ac8e38350a35732f29
Bar <barlivni@gmail.com>
2019-05-07 12:45:48 +0300

    paypal service

---
+++ app/src/main/java/com/example/royi/racebet/Config.java
@@ -0,0 +1,7 @@
+package com.example.royi.racebet;
+
+public class Config {
+public static String PAYPAL_KEY = "AdW-JacPG0TbD46HMtz7IfIFIQFKf5-4YgrAP-IY3ELd3GDT0c2qCPhg2jAqHfiICMB9TEgsvKL51xO2";
+public static int PalPalResultCode = 7171;
+}
+
