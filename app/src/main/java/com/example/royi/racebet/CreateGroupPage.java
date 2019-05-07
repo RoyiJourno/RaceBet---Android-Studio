ed76264f7d61c2b6ce8ac172dda498302e7a5ab5
RoyiJourno <royijourno@gmail.com>
2019-05-07 10:21:02 +0300

    CreateGroupPage done

--- app/src/main/java/com/example/royi/racebet/CreateGroupPage.java
+++ app/src/main/java/com/example/royi/racebet/CreateGroupPage.java
@@ -8,16 +8,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class CreateGroupPage extends AppCompatActivity {
 
     private Button btnCreateGroup, btnAddFriend;
     private EditText txtGroupName,txtBet,txtDuration,txtPhoneNumber;
+    FirebaseAuth mAuth;
+    DatabaseReference dRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        dRef = FirebaseDatabase.getInstance().getReference().child("Groups");
 
         btnCreateGroup = findViewById(R.id.btnCreateGroup);
         btnAddFriend=findViewById(R.id.btnAddFriend);
@@ -29,8 +38,8 @@ public class CreateGroupPage extends AppCompatActivity {
         btnCreateGroup.setOnClickListener(new View.OnClickListener() {//יוצר קבוצה ומכניס לדאטה בייס
             @Override
             public void onClick(View v) {
-                
-
+                Group group = new Group(dRef.push().getKey(),txtGroupName.getText().toString(),txtDuration.getText().toString(),null,mAuth.getUid(),null);
+                dRef.child(group.getGruopID()).setValue(group);
                 Intent intent=new Intent(CreateGroupPage.this,MainActivity.class);
                 startActivity(intent);
             }
